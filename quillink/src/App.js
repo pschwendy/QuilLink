7eb897163feefb6d6309c0d36fe21bbf1ba610db
YnotCode <40368753+YnotCode@users.noreply.github.com>
2021-08-02 12:44:24 -0400

    Added functional sidebar
    Still needs styling though

--- quillink/src/App.js
+++ quillink/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Projects from './pages/Projects';
 import SideBar from './components/SideBar';
+import Explore from './pages/Explore'
 import { Container, Row, Col } from 'react-bootstrap';
 import { Route, Switch } from 'react-router-dom';
 import './pages/css/main.css'
@@ -11,10 +12,15 @@ function App() {
     <div className="App">
       <Container style={{position: "relative"}}>
       <div id="page">
-        <SideBar id="the-side-bar"/>
+        <Col sm={4}>
+          <SideBar id="the-side-bar"/>
+        </Col>
+        <Col sm={8}>
         <Switch>
           <Route exact path='/' component={Projects}/>
+          <Route path='/explore' component={Explore}/>
         </Switch>
+        </Col>
       </div>
       </Container>
     </div>
--- quillink/src/components/SideBar.js
+++ quillink/src/components/SideBar.js
@@ -1,11 +1,27 @@
 import './css/SideBar.css';
+import {ListGroup, Container} from "react-bootstrap";
 
 function SideBar() {
     return (
-        <div>
+        /*<div>
             <div id="side-bar">
                 This is the side bar
             </div>
+        </div>*/
+        <div>
+        <Container fluid>
+        <ListGroup>
+            <ListGroup.Item action href="/">
+                Projects
+            </ListGroup.Item>
+            <ListGroup.Item action href="/explore">
+                Explore
+            </ListGroup.Item>
+            <ListGroup.Item action href="/trending">
+                Trending
+            </ListGroup.Item>
+        </ListGroup>
+        </Container>
         </div>
     );
 }
